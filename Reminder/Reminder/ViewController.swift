e344f9a9b7464191ff082bab7a9a9c5c3ee4b31a
Alysson Menezes Dodo <almenezes912@gmail.com>
2025-04-09 18:56:05 -0300

    start newProject

---
+++ Reminder/Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by ALYSSON MENEZES on 09/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
